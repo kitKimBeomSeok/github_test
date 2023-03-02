08efded846c30a0900e65f0a04ac40f9949ab15f
kitKimBeomSeok <126782965+kitKimBeomSeok@users.noreply.github.com>
2023-03-02 21:59:44 +0900

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello world!");
+        System.out.println("github is difficult");
+        System.out.println("github is difficult");
+    }
+
+
+}
